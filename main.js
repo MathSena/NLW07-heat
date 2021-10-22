e357da9b22783eeae12c146da918cf6531fcf57e
MathSena <MathSena07@hotmail.com>
2021-10-21 22:16:47 -0300

    Realizando ajuste no html e usando a API do GitHub

---
+++ main.js
@@ -0,0 +1,36 @@
+const LinksSocialMedia = {
+  github: 'mathsena',
+  youtube: 'mathsena',
+  facebook: 'mathsena',
+  instagram: 'mathsena',
+  twitter: 'mathsena'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+/* Pegando informações do GitHub */
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json()) // Pegando a url através do Fetch, jogando na promise para que transforme a resposta em um JSON
+    .then(data => {
+      // Pegando as informações e substituindo no DOM
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
